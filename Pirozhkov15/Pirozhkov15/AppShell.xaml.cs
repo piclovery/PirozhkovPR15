269ab3263bfeb28ef0203ee422e3d976e7d5aefc
piclove <93268417+piclovery@users.noreply.github.com>
2022-05-22 18:03:38 +0300

    Добавьте файлы проекта.

---
+++ Pirozhkov15/Pirozhkov15/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Pirozhkov15.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Pirozhkov15
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
